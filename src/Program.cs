afc9c0bb8be341c6f2f6f6cf89261ce6e7d81869
David Spörri <davepermen@hotmail.com>
2025-01-02 21:48:30 +0100

    ok

--- src/Program.cs
+++ src/Program.cs
@@ -1,29 +1,22 @@
 using Conesoft.Hosting;
-using Conesoft.Users;
-using Microsoft.AspNetCore.Http.Extensions;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Toolkit.Uwp.Notifications;
-using Serilog;
-using System.Web;
-
-var configuration = new ConfigurationBuilder().AddJsonFile(Conesoft.Hosting.Host.GlobalSettings.Path).Build();
-
-var wirepusher = configuration["wirepusher:base"]!;
-var wirepusherSecret = configuration["wirepusher:secret"]!;
-var conesoftSecret = configuration["conesoft:secret"]!;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder
+    .AddHostConfigurationFiles()
+    .AddUsersWithStorage()
+    .AddHostEnvironmentInfo()
+    .AddLoggingService()
+    ;
+
 builder.Services
     .AddCompiledHashCacheBuster()
-    .AddPeriodicGarbageCollection(TimeSpan.FromMinutes(5))
-    .AddHttpClient()
-    .AddUsersWithStorage();
+    .AddHttpClient();
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-builder.Services.AddSingleton(new UserTokenStorage(Conesoft.Hosting.Host.GlobalStorage / "Users"));
+builder.Services.AddSingleton<UserTokenStorage>();
 
 var app = builder.Build();
 
@@ -35,67 +28,13 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-app.MapGet("/notify", async ([FromQuery] string token, [FromQuery] string title, [FromQuery] string message, [FromQuery] string? imageUrl, [FromQuery] string? url) =>
-{
-    try
-    {
-        if (token != conesoftSecret)
-        {
-            return;
-        }
-
-        Log.Information("notifying {title}, {message}, {imageUrl}, {url}", title, message, imageUrl, url);
-
-        title = HttpUtility.HtmlDecode(title);
-        message = HttpUtility.HtmlDecode(message);
-
-        var wirepusherNotification = new QueryBuilder
-        {
-            { "title", title ?? "Conesoft Notification" },
-            { "message", message },
-            { "type", "Server" },
-            { "id", wirepusherSecret }
-        };
-
-        var localNotification = new ToastContentBuilder()
-            .AddText(title, AdaptiveTextStyle.Title)
-            .AddText(message);
-
-        if (url != null)
-        {
-            wirepusherNotification.Add("action", url);
-            localNotification = localNotification.SetProtocolActivation(new Uri(url));
-        }
-
-        if (imageUrl != null)
-        {
-            var path = Path.Combine(Path.GetTempPath(), Path.ChangeExtension(Path.GetRandomFileName(), Path.GetExtension(imageUrl)));
-
-            using var download = await new HttpClient().GetStreamAsync(imageUrl);
-            using var file = File.OpenWrite(path);
-            await download.CopyToAsync(file);
-
-            wirepusherNotification.Add("image_url", imageUrl);
-
-            localNotification = localNotification.AddHeroImage(new Uri(path, UriKind.Absolute));
-        }
-
-        await new HttpClient().GetAsync(wirepusher + wirepusherNotification.ToQueryString());
-
-        localNotification.Show();
-    }
-    catch (Exception ex)
-    {
-        Log.Error("error in /notify: {exception}", ex);
-        throw;
-    }
-});
-
-app.MapUsers();
-
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+
+app.MapStaticAssets();
+
+app.MapUsersWithStorage();
 
 app.UseRouting();
 
@@ -107,7 +46,8 @@ app.Run();
 
 
 
-record UserTokenStorage(Conesoft.Files.Directory Directory)
+class UserTokenStorage(HostEnvironment environment)
 {
+    public Conesoft.Files.Directory Directory => environment.Global.Storage / "Users";
     public Conesoft.Files.Directory For(string User) => Directory / User / "tokens";
 }
